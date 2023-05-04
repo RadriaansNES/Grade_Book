1660c8e330cf694ff3d03982f4a511e4c67324ab
Ryan Adriaans <Ryaan.1996s@hotmail.com>
2023-05-04 11:19:18 -0400

    Gradebook final grade assigning incorporated.

--- gradebook.py
+++ gradebook.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import pandas as pd
+import numpy as np
 
 HERE = Path(__file__).parent
 DATA_FOLDER = HERE / "data"
@@ -42,9 +43,86 @@ final_data = pd.merge(
     roster, hw_exam_grades, left_index=True, right_index=True,
 )
 
-## Merging quiz grades, using specified email as unique indentifier in lieu of NID/SID
+# Merging quiz grades, using specified email as unique indentifier in lieu of NID/SID
 final_data = pd.merge(
     final_data, quiz_grades, left_on="Email Address", right_index=True
 )
 
-print(final_data.head())
+# Replace NAN values with zero
+final_data = final_data.fillna(0)
+
+## Calculate exam scores 
+for n in range(1, 4):
+    final_data[f"Exam {n} Score"] = (
+        final_data[f"Exam {n}"] / final_data[f"Exam {n} - Max Points"]
+    )
+
+## Calculate homework scores via total score. Using homework xx (-) as reg
+homework_scores = final_data.filter(regex=r"^Homework \d\d?$", axis=1)
+homework_max_points = final_data.filter(regex=r"^Homework \d\d? -", axis=1)
+
+# Summing via columns
+sum_of_hw_scores = homework_scores.sum(axis=1)
+sum_of_hw_max = homework_max_points.sum(axis=1)
+final_data["Total Homework"] = sum_of_hw_scores / sum_of_hw_max
+
+# Calculate homework scores via average score
+hw_max_renamed = homework_max_points.set_axis(homework_scores.columns, axis=1)
+average_hw_scores = (homework_scores / hw_max_renamed).sum(axis=1)
+final_data["Average Homework"] = average_hw_scores / homework_scores.shape[1]
+
+# Take the maximum of two scoresets to determine which to use
+final_data["Homework Score"] = final_data[["Total Homework", "Average Homework"]].max(axis=1)
+
+## Calculate quiz scores. Same procedure as with homework scores but with using panda series to obtain quiz max score
+quiz_scores = final_data.filter(regex=r"^Quiz \d$", axis=1)
+quiz_max_points = pd.Series(
+    {"Quiz 1": 11, "Quiz 2": 15, "Quiz 3": 17, "Quiz 4": 14, "Quiz 5": 12}
+)
+
+sum_of_quiz_scores = quiz_scores.sum(axis=1)
+sum_of_quiz_max = quiz_max_points.sum()
+final_data["Total Quizzes"] = sum_of_quiz_scores / sum_of_quiz_max
+
+average_quiz_scores = (quiz_scores / quiz_max_points).sum(axis=1)
+final_data["Average Quizzes"] = average_quiz_scores / quiz_scores.shape[1]
+
+final_data["Quiz Score"] = final_data[
+    ["Total Quizzes", "Average Quizzes"]
+].max(axis=1)
+
+## Calculate and assign letter grades
+# Assign panda series to exam weights
+weightings = pd.Series(
+    {
+        "Exam 1 Score": 0.05,
+        "Exam 2 Score": 0.1,
+        "Exam 3 Score": 0.15,
+        "Quiz Score": 0.30,
+        "Homework Score": 0.4,
+    }
+)
+
+# Calculate final score via weightings
+final_data["Final Score"] = (final_data[weightings.index] * weightings).sum(
+    axis=1
+)
+# Turn final score into percent, rounding to the next highest integer
+final_data["Ceiling Score"] = np.ceil(final_data["Final Score"] * 100)
+
+# Assign grades via panda series
+grades = {
+    90: "A",
+    80: "B",
+    70: "C",
+    60: "D",
+    0: "F",
+}
+
+def grade_mapping(value):
+    for key, letter in grades.items():
+        if value >= key:
+            return letter
+        
+letter_grades = final_data["Ceiling Score"].map(grade_mapping)
+final_data["Final Grade"] = pd.Categorical(letter_grades, categories=grades.values(), ordered=True)
